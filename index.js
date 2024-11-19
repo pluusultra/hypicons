3353198ba2ce9615f686f5fb62b2839c71023063
Sergey Osipov <ultracrackerrr@gmail.com>
2024-11-19 19:56:53 +0700

    полный рефакторинг, сверстал header и 1 section

--- index.js
+++
@@ -1,6 +0,0 @@
-let buttonMenu = document.querySelector(".menu__btn");
-let menuList = document.querySelector(".menu__list");
-
-buttonMenu.addEventListener("click", () => {
-  menuList.classList.toggle("menu__list--visible");
-});
